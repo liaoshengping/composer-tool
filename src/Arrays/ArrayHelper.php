bef67acde8827278d2c4dbd6b24d3172d3563a71
廖 <1194008361@qq.com>
2020-04-08 09:19:23 +0800

    memo

--- src/Arrays/ArrayHelper.php
+++ src/Arrays/ArrayHelper.php
@@ -7,7 +7,7 @@ namespace Liaosp\Tool\Arrays;
 class ArrayHelper
 {
     /**
-     * 全局替换
+     * 全局替换多维数组  模版消息替换见PHPNOTE：https://github.com/liaoshengping/phpNote/blob/master/array/%E5%A4%9A%E7%BB%B4%E6%95%B0%E7%BB%84%E6%AD%A3%E5%88%99%E6%9B%BF%E6%8D%A2.php
      * @param $array
      * @param string $search
      * @param string $replace
