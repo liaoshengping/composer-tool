5931ce16e9d9c688727d1712c0749b7521badc2f
廖 <1194008361@qq.com>
2021-11-18 14:14:33 +0800

    添加数树

--- src/Arrays/ArrayHelper.php
+++ src/Arrays/ArrayHelper.php
@@ -43,4 +43,30 @@ class ArrayHelper
         }
         return $newArr;
     }
+
+
+    /**
+     * 一维数据数组生成数据树
+     * @param array $list 数据列表
+     * @param string $id 父ID Key
+     * @param string $pid ID Key
+     * @param string $son 定义子数据Key
+     */
+    public static function arr2tree($list, $id = 'id', $pid = 'pid', $son = 'sub')
+    {
+        list($tree, $map) = [[], []];
+        foreach ($list as $item) {
+            $map[$item[$id]] = $item;
+        }
+
+        foreach ($list as $item) {
+            if (isset($item[$pid]) && isset($map[$item[$pid]])) {
+                $map[$item[$pid]][$son][] = &$map[$item[$id]];
+            } else {
+                $tree[] = &$map[$item[$id]];
+            }
+        }
+        unset($map);
+        return $tree;
+    }
 }
